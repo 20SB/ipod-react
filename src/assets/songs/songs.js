047cf32fcd5dba46fbbe62a703f6ffa9133718e1
20SB <subhabiswal20@gmail.com>
2023-09-15 20:58:38 +0530

    ipod 1st commit

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./rasia.mp3";
+import music2 from "./rani-guri.mp3";
+import music3 from "./kenta-go.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
